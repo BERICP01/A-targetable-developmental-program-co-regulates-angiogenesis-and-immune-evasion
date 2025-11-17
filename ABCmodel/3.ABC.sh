16a94a702138cefbbb225725bfca97e384e2d243
Pietro Berico <153380512+BERICP01@users.noreply.github.com>
2025-11-17 16:43:36 -0500

    Update sample variable usage in prediction script

--- ABCmodel/3.ABC.sh
+++ ABCmodel/3.ABC.sh
@@ -1,17 +1,16 @@
 OUTPUT=ABC
 SAMPLE=("SKMEL-5" "SKMEL-147" "501mel")
-CURRENT_SAMPLE=${SAMPLE[$SLURM_ARRAY_TASK_ID-1]}
 
 ####Filter prediction
-mkdir -p $OUTPUT/${CURRENT_SAMPLE}/predictions_filtered
+mkdir -p $OUTPUT/${SAMPLE}/predictions_filtered
 python ABC-Enhancer-Gene-Prediction/workflow/scripts/filter_predictions.py \
---pred_file $OUTPUT/${CURRENT_SAMPLE}/predictions/EnhancerPredictionsAllPutative.tsv.gz \
---pred_nonexpressed_file $OUTPUT/${CURRENT_SAMPLE}/predictions/EnhancerPredictionsAllPutativeNonExpressedGenes.tsv.gz \
+--pred_file $OUTPUT/${SAMPLE}/predictions/EnhancerPredictionsAllPutative.tsv.gz \
+--pred_nonexpressed_file $OUTPUT/${SAMPLE}/predictions/EnhancerPredictionsAllPutativeNonExpressedGenes.tsv.gz \
 --only_expressed_genes true \
 --include_self_promoter false \
 --score_column ABC.Score \
 --threshold 0.02 \
---output_tsv_file $OUTPUT/${CURRENT_SAMPLE}/predictions_filtered/output.tsv \
---output_slim_tsv_file $OUTPUT/${CURRENT_SAMPLE}/predictions_filtered/output_slim.tsv \
---output_bed_file $OUTPUT/${CURRENT_SAMPLE}/predictions_filtered/output.bed \
---output_gene_stats_file $OUTPUT/${CURRENT_SAMPLE}/predictions_filtered/output_gene_stats.tsv
+--output_tsv_file $OUTPUT/${SAMPLE}/predictions_filtered/output.tsv \
+--output_slim_tsv_file $OUTPUT/${SAMPLE}/predictions_filtered/output_slim.tsv \
+--output_bed_file $OUTPUT/${SAMPLE}/predictions_filtered/output.bed \
+--output_gene_stats_file $OUTPUT/${SAMPLE}/predictions_filtered/output_gene_stats.tsv
