10a57de55f5ecd410f01386b1b4d1d1e3639f369
Pietro Berico <153380512+BERICP01@users.noreply.github.com>
2025-11-17 16:44:07 -0500

    Fix sample directory creation and pipseeker command

--- scRNAseq/1.pipseeker.sh
+++ scRNAseq/1.pipseeker.sh
@@ -1,11 +1,10 @@
 FASTQ='fastq'
 REFERENCE='PIPseeker_mm10'
 SAMPLE=("NTC-CD45-NEG" "NTC-CD45-POS" "SH-HOX-CD45-NEG" "SH-HOX-CD45-POS")
-CURRENT_SAMPLE=${SAMPLE[$SLURM_ARRAY_TASK_ID-1]}
 
 # pipseeker
 mkdir -p pipseeker
-mkdir -p pipseeker/B16F10-${CURRENT_SAMPLE}
-pipseeker full --fastq $FASTQ/B16F10-${CURRENT_SAMPLE} \
---star-index-path $REFERENCE --output-path pipseeker/B16F10-${CURRENT_SAMPLE} \
+mkdir -p pipseeker/B16F10-${SAMPLE}
+pipseeker full --fastq $FASTQ/B16F10-${SAMPLE} \
+--star-index-path $REFERENCE --output-path pipseeker/B16F10-${SAMPLE} \
 --chemistry V
